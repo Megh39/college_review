d901cd1f8fce36614042ee8c1e0fd0ac3e321e02
Megh39 <meghnanavati1@gmail.com>
2025-04-03 04:18:06 +0530

    Update Navigation.jsx

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import "./Navigation.css";
 import { Link } from "react-router-dom";
 const Navigation = () => {
+    
+    const handleLogout = () => {
+        localStorage.removeItem("user");
+        navigate("/login");
+    };
     return (
         <>
             <div className="navigationBar">
                 <div className="navLinks">
-                    {/* <Link to="/">Home</Link> */}
-                    {/* <Link to="/login">Login</Link> */}
                     <Link to="/dashboard">Dashboard</Link>
                     <Link to="/nirfranking">NIRF Rankings</Link>
                     <Link to="/colleges">Colleges</Link>
-
                     <Link to="/review">Write a Review</Link>
+                    <button className="logout-btn" onClick={handleLogout}>Logout</button>
+
                 </div>
             </div>
         </>
