8f193c8264e5d4e8bca02e6e97f0c033ec91ccb3
Megh39 <meghnanavati1@gmail.com>
2025-04-03 04:23:03 +0530

    logout button

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,25 +1,28 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navigation.css";
-import { Link } from "react-router-dom";
+
 const Navigation = () => {
-    
+    const navigate = useNavigate();
+
     const handleLogout = () => {
         localStorage.removeItem("user");
-        navigate("/login");
+        navigate("/register");
     };
-    return (
-        <>
-            <div className="navigationBar">
-                <div className="navLinks">
-                    <Link to="/dashboard">Dashboard</Link>
-                    <Link to="/nirfranking">NIRF Rankings</Link>
-                    <Link to="/colleges">Colleges</Link>
-                    <Link to="/review">Write a Review</Link>
-                    <button className="logout-btn" onClick={handleLogout}>Logout</button>
 
-                </div>
+    return (
+        <div className="navigationBar">
+            <div className="navLinks">
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/nirfranking">NIRF Rankings</Link>
+                <Link to="/colleges">Colleges</Link>
+                <Link to="/review">Write a Review</Link>
+                <Link to="#" onClick={handleLogout} >
+                    Logout
+                </Link>
             </div>
-        </>
-    )
-}
+        </div>
+    );
+};
+
 export default Navigation;
